4f742853e945a791f130c8bb816562dba2b054b4
jasurxamrayev <jasjasurbek4@gmail.com>
2022-05-11 11:24:36 +0500

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-layout view="hhh lpR fFf">
+
+    <q-header style="height:13%" reveal bordered class="bg-transparent text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div style="margin-top:30px;" class="fit row  justify-around items-center content-center">
+            <div>
+               <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/white.png">
+            </div>
+            <div width="70%">
+               <q-btn-dropdown unelevated class="bg-transparent mg" label="HOME" />
+               <q-btn-dropdown unelevated class="bg-transparent mg" label="PAGES" />
+               <q-btn-dropdown unelevated class="bg-transparent mg" label="SHOWCASES" />
+               <q-btn-dropdown unelevated class="bg-transparent mg" label="BLOG" />
+               <q-btn-dropdown unelevated class="bg-transparent mg" label="SHOP" />
+               <q-btn-dropdown unelevated class="bg-transparent mg" label="PRICING" />
+               <q-btn-dropdown unelevated class="bg-transparent mg" label="CONTECT US" />
+             
+            </div>
+          </div>
+            
+           
+          
+         
+        </q-toolbar-title>
+      </q-toolbar>
+
+    
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+<style scoped>
+.mg{
+  margin-left: 30px;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <q-img style="position:absolute; top:0%; height:500px;" src="	https://xtrm.foxthemes.me/wp-content/uploads/2018/12/Group-1.png">
+      <div style="width:50%;height:100%;background: #f569238d; " class="  row  justify-around items-start content-center">
+        <br><br><br>
+        <div>
+          <div class="text-h6 text-weight-light">ABOUT US</div><br>
+         <div style="font-size:75px" class=" text-weight-regular">Why You Should</div><br>
+         <div  class="text-weight-bolder bg-black" style=" font-size:60px;width:310px;padding-left:5px">Choose Us</div>
+        </div>    
+      </div>
+    </q-img>
+    <div style="margin-top:500px"></div>
+    <div class="row  justify-around  items-center content-center">
+    <!-- card1 -->
+      <q-card style="width: 21%" class="my-card card-hover"><br>
+        <div class="text-h2 text-weight-medium" style="margin-left:100px ">01.</div>
+      <q-card-section>
+        <div class="text-h6">We're Super Talented Team</div><br>
+        <div class="text-subtitle1 text-center text-weight-light">Nam porttitor blandit <br> accumsan. Ut vel dictum sem, <br> a pretium dui. In malesuada.</div><br>
+      </q-card-section><br>
+    </q-card>
+        <!-- card2 -->
+      <q-card style="width: 21%" class="my-card card-hover border"><br>
+        <div class="text-h2 text-weight-medium" style="margin-left:100px ">02.</div>
+      <q-card-section>
+        <div class="text-h6">We Have Best Equipment</div><br>
+        <div class="text-subtitle1 text-center text-weight-light">In hac habitasse platea <br> dictumst. Vivamus adipiscing <br>fermentum quam volutpat.</div><br>
+      </q-card-section><br>
+    </q-card>
+        <!-- card1 -->
+      <q-card style="width: 21%" class="my-card card-hover"><br>
+        <div class="text-h2 text-weight-medium" style="margin-left:100px ">03.</div>
+      <q-card-section>
+        <div class="text-h6">We’re The Best Company</div><br>
+        
+        <div class="text-subtitle1 text-center text-weight-light">Cras quis nulla commodo, <br>aliquam lectus sed, blandit<br> augue. Cras ullamcorper.</div><br>
+      </q-card-section><br>
+    </q-card>
+        <!-- card1 -->
+      <q-card style="width: 21%" class="my-card card-hover"><br>
+        <div class="text-h2 text-weight-medium" style="margin-left:100px ">04.</div>
+      <q-card-section>
+        <div class="text-h6">We Have Coffee and Cookies</div><br>
+        <div class="text-subtitle1 text-center text-weight-light">We Have Coffee and Cookies <br> fringilla rutrum. Aenean <br> sollicitudin, erat a elem.</div><br>
+      </q-card-section><br>
+    </q-card>
+    </div>
+  </div>
+</template>
+
+
+
+<style scoped>
+.card-hover{
+  transition: 0.3s;
+}
+.card-hover:hover{
+  background: #f56923;
+  color: #fff;
+  transition: 0.3s;
+}
+.border{
+  border: 10px solid #f56923;
+}
+</style>
+
